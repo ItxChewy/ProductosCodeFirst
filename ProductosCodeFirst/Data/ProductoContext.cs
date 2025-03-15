6079665f409a067083b788f91aa19f50dc122587
ItxChewy <aarongarcia1906@gmail.com>
2025-03-15 12:18:25 +0100

    Add project files.

---
+++ ProductosCodeFirst/Data/ProductoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductosCodeFirst.Models;
+
+namespace ProductosCodeFirst.Data
+{
+    public class ProductoContext:DbContext
+    {
+        public ProductoContext(DbContextOptions<ProductoContext>options)
+            : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        
+    }
+}
---
+++ ProductosCodeFirst/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductosCodeFirst.Models
+{
+    [Table("Producto")]
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        public int Precio { get; set; }
+        [Required]
+        public int Stock { get; set; }
+    }
+}
